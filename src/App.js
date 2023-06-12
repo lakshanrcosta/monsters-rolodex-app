8600298b9ef761551955cbdc95136dd8ebeab110
Lakshan Costa <lakshanrcosta@gmail.com>
2023-06-13 00:34:29 +0300

    Setting up monster search functiionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchText: '',
+    };
+  }
+
+  onSearchHandler = (event) => {
+    const searchText = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchText };
+    });
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        }),
+      );
+  }
+
+  render() {
+    const { monsters, searchText } = this.state;
+    const { onSearchHandler } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchText);
+    });
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search monsters'
+          onChange={onSearchHandler}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class CardList extends Component {
+  render() {
+    return <h1>Hei I am the card list</h1>;
+  }
+}
+
+export default CardList;
